343db8be55d69e925ac8eb1185b422c2626821d3
Soner Ozenc <sonerozenc@gmail.com>
2013-03-26 14:04:26 +0000

    Strings exercise

---
+++ strings.rb
@@ -0,0 +1,17 @@
+# Ask question
+puts "Hello Charles, what are you doing?"
+
+# Get Charlie's activity via keyboard
+activity = gets.chomp.downcase
+
+# Print Charlie's activity
+puts "Charles is #{activity}"
+
+# Ask for age
+puts "Charles, how old are you?"
+
+# Get age
+age = gets.chomp
+
+# Print activity and age
+puts "Charles is #{activity} and is #{age} age"
